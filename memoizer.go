0c4917d2790a29e45474d94679faa4ad932e7bd3
Arjun Gill <agillgilla@gmail.com>
2018-04-25 22:07:21 -0700

    Got memoizer to pass

--- memoizer.go
+++ memoizer.go
@@ -2,6 +2,8 @@ package memoizer
 
 import (
 	"github.com/61c-teach/sp18-proj5"
+	"hash/crc64"
+	"fmt"
 )
 
 /* The simplest possible implementation that does anything interesting.
@@ -10,8 +12,34 @@ and the classifier. You will need to improve this to use the cache effectively. 
 func Memoizer(memHandle proj5.MnistHandle, classHandle proj5.MnistHandle, cacheHandle proj5.CacheHandle) {
 
 	for req := range memHandle.ReqQ {
-		classHandle.ReqQ <- req
+		crcTable := crc64.MakeTable(crc64.ECMA)
+    	reqKey := crc64.Checksum(req.Val, crcTable)
+    	//fmt.Printf("Checksum: %x \n", reqKey)
+
+    	cacheReadReq := proj5.CacheReq{false, reqKey, 0, req.Id} 
+    	cacheHandle.ReqQ <- cacheReadReq
+    	
+    	cacheResp := <-cacheHandle.RespQ
+
+    	if cacheResp.Exists { // Request is already in cache, read it from memory
+    		if cacheResp.Id == cacheReadReq.Id { // Id is correct (for out of order channel responses)
+    			finalResp := proj5.MnistResp{cacheResp.Val, cacheReadReq.Id, nil}
+    			memHandle.RespQ <- finalResp
+    		} else { // ERROR!
+    			fmt.Printf("%s", "ERROR! Cache Response ID doesn't match Cache Request ID!")
+    		}
+    	} else { // Request is not in cache, calculate it and memoize it
+    		classHandle.ReqQ <- req
+    		finalResp := <-classHandle.RespQ
+
+    		cacheWriteReq := proj5.CacheReq{true, reqKey, finalResp.Val, req.Id}
+    		cacheHandle.ReqQ <- cacheWriteReq
+
+    		memHandle.RespQ <- finalResp
+    	}
+
+		/*classHandle.ReqQ <- req
 		resp := <-classHandle.RespQ
-		memHandle.RespQ <- resp
+		memHandle.RespQ <- resp*/
 	}
 }
