1e9b6035d887d7d8149e150e3f61fa661f106f9c
Arjun Gill <agillgilla@gmail.com>
2018-05-01 12:33:31 -0700

    Debugging classCrash

--- proj5-testing/mock_test.go
+++ proj5-testing/mock_test.go
@@ -313,6 +313,31 @@ func checkClassCrash(handle proj5.MnistHandle, ims []GoMNIST.RawImage, t *testin
 		reqID++
 	}
 
+	// Check 10 cached images
+    for i := 0; i < 100; i++ {
+    	// Get a random index of a result that should be cached
+		seenPicIndex := rand.Intn(whenFail - 1)
+
+		// The whenFail'th miss should have a classifier crash, but it should work fine 
+		// since a previous element should be cached
+		handle.ReqQ <- proj5.MnistReq{ims[seenPicIndex], reqID}
+		resp, ok := <-handle.RespQ
+		if !ok {
+			t.Error("Memoizer exited after classifier crashed.")
+		}
+
+		if resp.Err != nil {
+			t.Error("Memoizer reported an error after classifier crashed, but request was in cache!")
+			t.FailNow()
+		}
+
+		if resp.Val != exp[seenPicIndex] {
+			t.Error("Memoizer returned incorrect value after classifier crashed, but request was in cache!")
+		}
+
+		reqID++
+	}
+
 }
 
 // Bad mock cache.  Crashes on the whenFail'd request
